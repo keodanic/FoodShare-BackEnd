c1276ce4f62444df23e933d510a0695cdaf2b057
LPeter-nm <pg.nasmatos@outlook.com>
2025-02-20 17:03:52 -0300

    feat: vulnerable crud

--- app/controllers/vulnerables_controller.ts
+++ app/controllers/vulnerables_controller.ts
@@ -1,4 +1,44 @@
-// import type { HttpContext } from '@adonisjs/core/http'
+import type { HttpContext } from '@adonisjs/core/http'
+import Vulnerable from '#models/vulnerable'
+import { createVunerableValidator, updateVunerableValidator } from '#validators/vulnerable'
 
 export default class VulnerablesController {
+  async index() {
+    const vulnerables = await Vulnerable.query()
+    return vulnerables
+  }
+
+  async store({ request }: HttpContext) {
+    const { name, email, password, cpf } = await request.validateUsing(createVunerableValidator)
+    const vulnerable = await Vulnerable.create({ name, email, password, cpf })
+    return vulnerable
+  }
+
+  async show({ params, response }: HttpContext) {
+    try {
+      const vulnerable = await Vulnerable.findByOrFail('id', params.id)
+      await vulnerable.load('objects')
+      return vulnerable
+    } catch (error) {
+      return response.status(404).json({ message: 'Vulnerable not found' })
+    }
+  }
+
+  async update({ params, request }: HttpContext) {
+    const vulnerable = await Vulnerable.findBy('id', params.id)
+    const { name, password } = await request.validateUsing(updateVunerableValidator)
+    vulnerable!.merge({ name, password })
+    await vulnerable!.save()
+    return vulnerable
+  }
+
+  async destroy({ params, response }: HttpContext) {
+    try {
+      const vulnerable = await Vulnerable.findByOrFail('id', params.id)
+      await vulnerable.delete()
+      return response.status(203)
+    } catch (error) {
+      return response.status(404).json({ message: 'Vulnerable not found' })
+    }
+  }
 }
--- start/routes.ts
+++ start/routes.ts
@@ -1,16 +1,4 @@
-/*
-|--------------------------------------------------------------------------
-| Routes file
-|--------------------------------------------------------------------------
-|
-| The routes file is used for defining the HTTP routes.
-|
-*/
-
+const VulnerablesController = () => import('#controllers/vulnerables_controller')
 import router from '@adonisjs/core/services/router'
 
-router.get('/', async () => {
-  return {
-    hello: 'world',
-  }
-})
+router.resource('vulnerable', VulnerablesController).apiOnly()
